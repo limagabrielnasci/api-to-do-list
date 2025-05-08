13ffa653f9b009f36402229eef26a1fdda751e9e
Gabriel Nascimento <limagabrielnasci@gmail.com>
2025-05-08 19:11:08 -0300

    test: implement automated tests and required changes

--- src/main/java/br/com/limagabrielnasci/challenge_todolist/entity/Todo.java
+++ src/main/java/br/com/limagabrielnasci/challenge_todolist/entity/Todo.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity // marca a classe como entidade JPA
 @Table(name = "todos") // mapeia a entidade para a tabela "todos"
@@ -14,10 +15,21 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // id gerado automaticamente pelo banco
     private Long id;
 
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String description;
+    
     private boolean completed;
     private int priority;
+
+    public Todo(String name, String description, boolean completed, int priority) {
+        this.name = name;
+        this.description = description;
+        this.completed = completed;
+        this.priority = priority;
+    }
 
     public Long getId() {
         return id;
