710ea32bd19ed8b352d1c05e8f48a2073c712893
Gabriel Nascimento <limagabrielnasci@gmail.com>
2025-05-08 12:05:21 -0300

    initial commit

---
+++ src/main/java/br/com/limagabrielnasci/challenge_todolist/ChallengeTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.limagabrielnasci.challenge_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/limagabrielnasci/challenge_todolist/ChallengeTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.limagabrielnasci.challenge_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
